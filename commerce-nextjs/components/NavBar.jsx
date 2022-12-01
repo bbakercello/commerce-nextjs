c8a9a209cc3d90d593ad0b309e6e82f5842ab4f4
bbakercello <bbakercello@gmail.com>
2022-12-01 07:51:54 -0500

    user auth interface on nav is functional

--- commerce-nextjs/components/NavBar.jsx
+++ commerce-nextjs/components/NavBar.jsx
@@ -6,19 +6,8 @@ import SignInLogo from "./logos/SignInLogo";
 import SearchLogo from "./logos/SearchLogo";
 import { useUser } from "@auth0/nextjs-auth0";
 import Link from "next/link";
-import {
-  Collapse,
-  Container,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-} from "reactstrap";
+import { Menu } from "@headlessui/react";
+
 const NavBar = () => {
   const { user, isLoading } = useUser();
   return (
@@ -32,51 +21,55 @@ const NavBar = () => {
         <SearchLogo />
       </div>
       <div className="flex flex-col justify-center pl-2 pr-2">
-        <SignInLogo />
         {!isLoading && !user && (
           <Link href="/api/auth/login" className="btn btn-primary btn-margin">
-            Log in
+            <SignInLogo />
           </Link>
         )}
         {user && (
-          <UncontrolledDropdown nav inNavbar data-testid="navbar-menu-desktop">
-            <DropdownToggle nav caret id="profileDropDown">
-              <img
-                src={user.picture}
-                alt="Profile"
-                className="nav-user-profile rounded-circle"
-                width="50"
-                height="50"
-                decode="async"
-                data-testid="navbar-picture-desktop"
-              />
-            </DropdownToggle>
-            <DropdownMenu>
-              <DropdownItem header data-testid="navbar-user-desktop">
-                {user.name}
-              </DropdownItem>
-              <DropdownItem className="dropdown-profile" tag="span">
-                <Link
-                  href="/profile"
-                  icon="user"
-                  testId="navbar-profile-desktop"
-                >
-                  Profile
-                </Link>
-              </DropdownItem>
-              <DropdownItem id="qsLogoutBtn">
-                <Link
-                  href="/api/auth/logout"
-                  icon="power-off"
-                  testId="navbar-logout-desktop"
-                >
-                  Log out
-                </Link>
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
+          <div>
+            <div>
+              <Menu>
+                <Menu.Button className="text-slate-700 pl-6 relative">
+                  <img
+                    src={user.picture}
+                    alt="Profile"
+                    className="nav-user-profile rounded-circle"
+                    width="50"
+                    height="50"
+                    decode="async"
+                    data-testid="navbar-picture-desktop"
+                  />
+                </Menu.Button>
+                <Menu.Items className="text-neutral-200 rounded-lg flex flex-col bg-slate-900 absolute">
+                  <Menu.Item>{({ active }) => <p> {user.name}</p>}</Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        href="/profile"
+                        icon="user"
+                        testId="navbar-profile-desktop"
+                      >
+                        Profile
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item disabled>
+                    <Link
+                      href="/api/auth/logout"
+                      icon="power-off"
+                      testId="navbar-logout-desktop"
+                    >
+                      Log out
+                    </Link>
+                  </Menu.Item>
+                </Menu.Items>
+              </Menu>
+            </div>
+          </div>
         )}
       </div>
+
       <div className="flex flex-col justify-center pl-2 pr-2">
         <CartLogo />
       </div>
