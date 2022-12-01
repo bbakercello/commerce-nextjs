a708f99fd6563f9350fe3426be78947711ded7db
bbakercello <bbakercello@gmail.com>
2022-12-01 10:05:56 -0500

    adding prisma and postgresql for data

---
+++ commerce-nextjs/components/Seasonal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Seasonal = () => {
+  return (
+    <div>
+      <h1>Fall Style Guide</h1>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Seasonal
